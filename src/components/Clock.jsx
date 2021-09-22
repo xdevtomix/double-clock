c6fdbdab9a9106ee328245c556ba324f54115b04
tomi <dev.dreissiger@gmail.com>
2021-09-22 15:08:10 +0200

    styled components -ready

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -6,20 +6,41 @@ export default function Clock() {
     const [minutesDeg, setMinutesDeg] = useState(0);
     const [secondsDeg, setSecondsDeg] = useState(0);
 
+    const [hour, setHour] = useState('');
+    const [minute, setMinute] = useState('');
+    const [ampm, setAmpm] = useState('');
+    const [day, setDay] = useState('');
+    const [month, setMonth] = useState('');
+    const [year, setYear] = useState('');
+
     const [theme, setTheme] = useState('');
 
     useEffect(() => {
         let intervalId = -1;
+        let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
         const runClock = () => {
             const date = new Date();
-            const hDeg = date.getHours() * 30;
-            const mDeg = date.getMinutes() * 6;
-            const sDeg = date.getSeconds() * 6;
+            const h = date.getHours();
+            const mi = date.getMinutes();
+            const s = date.getSeconds();
+            const d = date.getDate();
+            const mo = date.getMonth();
+            const y = date.getFullYear();
+            const hDeg = h * 30;
+            const mDeg = mi * 6;
+            const sDeg = s * 6;
 
             setHoursDeg(hDeg + mDeg / 12);
             setMinutesDeg(mDeg);
             setSecondsDeg(sDeg);
+
+            setHour(h < 10 ? `0${h}` : `${h}`);
+            setMinute(mi < 10 ? `0${mi}` : `${mi}`);
+            setAmpm(h < 12 ? 'AM' : 'PM');
+            setDay(d);
+            setMonth(months[mo]);
+            setYear(y);
         };
 
         intervalId = setInterval(runClock, 1000);
@@ -29,74 +50,6 @@ export default function Clock() {
         return () => {
             clearInterval(intervalId);
         };
-    }, []);
-
-    useEffect(() => {
-        /*==================== CLOCK & DATE TEXT ====================*/
-        const textHour = document.getElementById('text-hour'),
-            textMinutes = document.getElementById('text-minutes'),
-            textAmPm = document.getElementById('text-ampm'),
-            //   dateWeek = document.getElementById('date-day-week'),
-            dateDay = document.getElementById('date-day'),
-            dateMonth = document.getElementById('date-month'),
-            dateYear = document.getElementById('date-year')
-
-        const clockText = () => {
-            let date = new Date()
-
-            let hh = date.getHours(),
-                ampm,
-                mm = date.getMinutes(),
-                day = date.getDate(),
-                // dayweek = date.getDay(),
-                month = date.getMonth(),
-                year = date.getFullYear()
-
-            // We change the hours from 24 to 12 hours and establish whether it is AM or PM
-            if (hh >= 12) {
-                hh = hh - 12
-                ampm = 'PM'
-            } else {
-                ampm = 'AM'
-            }
-
-            // We detect when it's 0 AM and transform to 12 AM
-            if (hh == 0) { hh = 12 }
-
-            // Show a zero before hours
-            if (hh < 10) { hh = `0${hh}` }
-
-            // Show time
-            textHour.innerHTML = `${hh}:`
-
-            // Show a zero before the minutes
-            if (mm < 10) { mm = `0${mm}` }
-
-            // Show minutes
-            textMinutes.innerHTML = mm
-
-            // Show am or pm
-            textAmPm.innerHTML = ampm
-
-            // If you want to show the name of the day of the week
-            // let week = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
-
-            // We get the months of the year and show it
-            let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-
-            // We show the day, the month and the year
-            dateDay.innerHTML = day
-            // dateWeek.innerHTML = `${week[dayweek]}`
-            dateMonth.innerHTML = `${months[month]},`
-            dateYear.innerHTML = year
-        }
-        setInterval(clockText, 1000) // 1000 = 1s
-
-        clockText();
-
-        const iconTheme = 'bxs-sun' // sun moon
-
-        return () => { };
     }, []);
 
     useEffect(() => {
@@ -141,16 +94,14 @@ export default function Clock() {
                 </Analog>
 
                 <Digital data-component="digital">
-                    <DigitalTime data-component="digital-time" className="clock__text">
-                        <div className="clock__text-hour" id="text-hour"></div>
-                        <div className="clock__text-minutes" id="text-minutes"></div>
-                        <div className="clock__text-ampm" id="text-ampm"></div>
+                    <DigitalTime data-component="digital-time">
+                        <div>{hour}:</div>
+                        <div>{minute}</div>
+                        <span>{ampm}</span>
                     </DigitalTime>
 
-                    <DigitalDate data-component="digital-date" className="clock__date">
-                        <span id="date-day"></span>
-                        <span id="date-month"></span>
-                        <span id="date-year"></span>
+                    <DigitalDate data-component="digital-date">
+                        <div>{`${day} ${month}, ${year}`}</div>
                     </DigitalDate>
                 </Digital>
             </TwoClocks>
@@ -260,11 +211,27 @@ const Digital = styled.div`
 `;
 
 const DigitalTime = styled.div`
+    display: flex;
+    justify-content: center;
 
+    > div {
+        font-size: var(--biggest-font-size);
+        font-weight: var(--font-medium);
+        color: var(--title-color);
+    }
+
+    > span {
+        font-size: var(--tiny-font-size);
+        color: var(--title-color);
+        font-weight: var(--font-medium);
+        margin-left: var(--mb-0-25);
+    }
 `;
 
 const DigitalDate = styled.div`
-
+    text-align: center;
+    font-size: var(--small-font-size);
+    font-weight: var(--font-medium);
 `;
 
 const ThemeSwitcher = styled.div`
